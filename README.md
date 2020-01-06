cde42ef19db4f3021813b6e94027393ce03c5232
Xavier Ramirez <xavier@dreamcode.io>
2020-01-05 20:22:11 -0500

    feat(SSBP-4): Add checkedLabel output

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# pr-has-label-action
+# Does PR has the label?
+
+Do you want to know if a PR has a particular label? You can use this action to easily chech that, and use the output as you need:
+
+Example:
+
+```
+name: CI
+on: 
+  pull_request:
+    types: [opened, labeled]
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: Dreamcodeio/pr-has-label-action@master
+      with:
+        label: PROVISION:FEATURE_BRANCH
+```
--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const github = require("@actions/github");
 
 const label = core.getInput("label");
 
-core.setOutput("label", label);
+core.setOutput("checkedLabel", label);
 const hasLabel = github.context.payload.pull_request.labels.some(
   item => item.name === label
 );
 core.setOutput("hasLabel", hasLabel);
 
 console.log(`Does 'pull_request' has '${label}' label?: ${hasLabel}`);
-console.log(`You can use this output as 'steps.<step id>.outputs.hasLabel'`);
+console.log(`You can use this output as 'steps.<step id>.outputs.hasLabel' and 'steps.<step id>.outputs.checkedLabel' `);
